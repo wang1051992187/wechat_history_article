80198c77fff53bc600422dac2a763b9ed41d29d9
王揆豪 <wangkuihao@andlinks.com>
2018-07-31 14:13:56 +0800

    获取历史记录的URL

--- get_history_url.py
+++ get_history_url.py
@@ -1,10 +1,9 @@
-from .article_detail import *
-from .RedisQueue import RedisQueue
-from .common import *
+from RedisQueue import RedisQueue
+from common import *
 from pypinyin import pinyin, lazy_pinyin, Style
 
 begin_url = '''
-https://mp.weixin.qq.com/mp/profile_ext?action=getmsg&__biz=MjM5MzU4NDkzOA==&f=json&offset={}&count=10&is_ok=1&scene=124&uin=MTMzMDE2MDc0NA%3D%3D&key=1d3570e9b8c45238b8a0db7cf4c40e4f105b365727bd0ddf56a2cb43aa4513c77f9f39dfe9a1244ce41bc9cd3619aa33c477af77276e55e0ba8d07aa64020fba92ee242f1a58e56740c5c669893938fc&pass_ticket=qf07TCU8fHwcY0KEmh14RExqCSginGN%2BfzUIgMYpfoZ9GyEsHlch5bSOaK8hptp2&wxtoken=&appmsg_token=967_z86Ewlm2ub6hzQuCiP8MfKC_GzehTglYPZDJfQ~~&x5=0&f=json%0A
+https://mp.weixin.qq.com/mp/profile_ext?action=getmsg&__biz=MzA3ODU2NjAxOQ==&f=json&offset={}&count=10&is_ok=1&scene=124&uin=MTMzMDE2MDc0NA%3D%3D&key=1d3570e9b8c452388fd135a23f3f3e09cc5bffc8275573e2acb127d50da54a231497048862d19c79c135e4b8916fd9f24a1ba6079b9bb039130cf49e264e4d118cf43f48bcd6d73e08932574f044a7e7&pass_ticket=TGTbEK6XyMIBkeZtnmHKJNN3IvRtq11v%2FxnwTZpvxKxWna0o3Usm93B4FYO0c8vh&wxtoken=&appmsg_token=967_mkzMEVO06r1lIB36o4ebaLaYOoN3JFDR7fyA1A~~&x5=0&f=json%0A
 '''
 
 
@@ -14,7 +13,7 @@ def get_all_url(url, nickname):
     if nickname is None:
         print("空")
         return
-    q = RedisQueue('wechat_url_'+ nickname_pinyin.strip())
+    q = RedisQueue('wechat_url_' + nickname_pinyin.strip())
     json_str = get_page_detail(url)
     json_re = parse_page_index(json_str)
     general_msg_list = parse_page_index(json_re['general_msg_list'])
@@ -58,7 +57,7 @@ def get_all_url(url, nickname):
                 q.put(data2)
                 print(data2)
         except KeyError as e:
-            print("error",e)
+            print("error", e)
 
     # 获取 next_offset
     if len(general_msg_list['list']) < 10:
@@ -67,7 +66,7 @@ def get_all_url(url, nickname):
 
 
 def url_circulate(next_offset):
-    nickname = " 南京银行无锡分行"
+    nickname = " 南京银行盐城分行"
     next_url = begin_url.format(next_offset)
     print(next_url)
     next_offset = get_all_url(next_url,nickname)
